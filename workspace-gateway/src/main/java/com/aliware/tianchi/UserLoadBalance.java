a5a8122a2b75fd13674836e3a74fdd18c7baf9db
kanbo123 <52125963+kanbo123@users.noreply.github.com>
2019-07-06 17:19:01 +0800

    Update UserLoadBalance.java

--- workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
+++ workspace-gateway/src/main/java/com/aliware/tianchi/UserLoadBalance.java
@@ -58,8 +58,6 @@ public class UserLoadBalance implements LoadBalance{
                     if(avgSpendTimeMaxIndex == index){
                         // 耗时最大的并发数减半
                         permits = permits/2;
-                    }else{
-                        permits = permits+50;
                     }
                     if(permits>0){
                         hasPermitArr.add(index);
